e02be2c222380ae737202ed16122250bdcbc70c1
ArtShoo <artem.bobcow@yandex.ru>
2020-06-15 14:12:25 +0300

    Version 1.5

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return < Preloader />
+    }
+
     return (
         <div >
             <div>
                 <img src="https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png"/>
             </div>
             <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large} />
                 avatar + description
             </div>
         </div>
