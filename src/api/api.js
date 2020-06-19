261b5076a1cd385161baedf1ce1ff7076900b695
ArtShoo <artem.bobcow@yandex.ru>
2020-06-19 13:14:11 +0300

    Version 1.5

--- src/api/api.js
+++ src/api/api.js
@@ -8,23 +8,30 @@ const instance = axios.create({
     }
 });
 
-
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data;
             });
+    },
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
+    },
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
+    },
+    getProfile(userId) {
+        return instance.get(`profile/` + userId);
+
     }
 }
 
+export const authAPI = {
+    me(){
+        return instance.get(`auth/me`)
+    }
 
-
-/* export const getUsers2 = (currentPage = 1, pageSize = 10) => {
-    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-        .then(response => {
-            return response.data;
-        });
-} */
+}
 
 
