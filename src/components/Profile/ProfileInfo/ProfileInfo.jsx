0d0d6d70d6a486f2a20f4942b14ed242abd6e97a
ArtShoo <artem.bobcow@yandex.ru>
2020-07-06 13:38:53 +0300

    Version 1.5

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -8,12 +9,12 @@ const ProfileInfo = (props) => {
     }
     return (
         <div >
-            <div>
+{/*            <div>
                 <img src="https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png"/>
-            </div>
+            </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                avatar + description
+                <ProfileStatus />
             </div>
         </div>
     );
